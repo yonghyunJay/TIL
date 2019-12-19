6f2fdd93c33c14a5c95e0b0d860373c1836cac37
yonghyun Jeong <jayjay@kakao.com>
2019-12-19 20:51:05 +0900

    Modify op_gg.py

--- python/flask/op_gg.py
+++ python/flask/op_gg.py
@@ -19,7 +19,7 @@ def opgg():
 
     win = soup.select_one('#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins')
 
-    return render_template('opgg.html', name = userName, tier = tier.text, win = win.text, url = url)
+    return render_template('opgg.html', name = userName, tier = tier.text, win = win.text[: - 1], url = url)
 
 if __name__ == ("__main__"):
     app.run(debug=True)
